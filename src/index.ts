e734f3625ed64b4ebd951ce3f385baa1059fb902
Dmitry Rogozhny <drogozhny@gmail.com>
2022-12-01 15:43:27 +0100

    Get data directly from adventofcode

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-import { readFile } from './utils';
+import dotenv from 'dotenv';
+import { getLines, fetchData } from './utils';
 
 import { day1part1, day1part2 } from './day-1'
 
-const data = readFile('./data/day1.txt');
+dotenv.config();
 
-console.log(day1part1(data));
-console.log(day1part2(data));
+async function run() {
+    const day1Data = await fetchData(1);
+    const data = getLines(day1Data);
+    
+    console.log(day1part1(data));
+    console.log(day1part2(data));
+
+}
+
+run();
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from "fs";
+import fetch from 'node-fetch';
 
 /**
  * Reads the file and retuns a list of text lines.
@@ -6,4 +7,29 @@ import { readFileSync } from "fs";
  * @param separator separator for lines
  * @returns A list of lines
  */
-export const readFile = (path: string, separator = "\n") => readFileSync(path).toString().split(separator);
+export const readFile = (path: string) => getLines(readFileSync(path).toString());
+
+/**
+ * Returns the data from the advent of code site for the specified day
+ * @param day The target day of the advent
+ */
+export const fetchData = async (day: number) => {
+    const cookie = process.env.COOKIE;
+
+    if (cookie === undefined) {
+        throw new Error('Cookie is required for getting data. Please check the COOKIE value in the .env file.');
+    }
+
+    const response = await fetch(`https://adventofcode.com/2022/day/${day}/input`, { headers: {cookie}});
+    const body = await response.text();
+    
+    return body;
+}
+
+/**
+ * Splits data into lines.
+ * @param data String with lines of data
+ * @param separator Optional separator. By default, new line is used.
+ * @returns List with lines of data
+ */
+export const getLines = (data: string, separator = '\n') => data.split(separator);
