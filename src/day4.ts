ebde13322fafb6220fa60a30bf9cdc79b7374d66
Dmitry Rogozhny <drogozhny@gmail.com>
2022-12-04 20:21:51 +0100

    Solve day4

---
+++ src/day4.ts
@@ -0,0 +1,65 @@
+interface Range {
+  from: number
+  to: number
+}
+
+// https://adventofcode.com/2022/day/4
+export function day4part1 (data: string[]): number {
+  // parse data from strings into Range objects
+  const rangePairs = data.map(getRanges)
+  // sort range pairs, so the first range from is always smaller or equal to the second from.
+  // This makes further comparision simpler
+  rangePairs.forEach((rangePair) => rangePair.sort((a, b) => a.from - b.from))
+
+  const fullyContainedRanges = rangePairs.filter(([range1, range2]) => {
+    // because of previous sorting, r1.from will always be smaller or equal to r2.from,
+    // so we only need to check cases when r1.from === r2.from (in any case 2 ranges overlap)
+    // and when the first range overlaps the second, i.e. r1.to is more the r2.to
+    if (range1.from === range2.from) {
+      return true
+    } else {
+      return range1.to >= range2.to
+    }
+  })
+
+  return fullyContainedRanges.length
+}
+
+// https://adventofcode.com/2022/day/4#part2
+export function day4part2 (data: string[]): number {
+  // parse data from strings into Range objects
+  const rangePairs = data.map(getRanges)
+  // sort range pairs, so the first range from is always smaller or equal to the second from.
+  // This makes further comparision simpler
+  rangePairs.forEach((rangePair) => rangePair.sort((a, b) => a.from - b.from))
+
+  // because of previous sorting, r1.from will always be smaller or equal to r2.from,
+  // so we need to check only the right side of range, i.e. r1.to
+  const overlappingRanges = rangePairs.filter(([range1, range2]) => {
+    return range1.to >= range2.from
+  })
+
+  return overlappingRanges.length
+}
+
+/**
+ * Parses the string and returns two ranges
+ * @param rangesStr The string in the a-b,y-z format
+ * @returns The array with two ranges
+ */
+function getRanges (rangesStr: string): Range[] {
+  const [range1Str, range2Str] = rangesStr.split(',')
+
+  return [getRange(range1Str), getRange(range2Str)]
+}
+
+/**
+ * Parses the range string and retuns a Range object
+ * @param rangeStr The string in the a-b format, where a and b are two integers
+ * @returns
+ */
+function getRange (rangeStr: string): Range {
+  const [fromStr, toStr] = rangeStr.split('-')
+
+  return { from: parseInt(fromStr, 10), to: parseInt(toStr, 10) }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
 import dotenv from 'dotenv'
-import { day3part1, day3part2 } from './day3'
+import { day4part1, day4part2 } from './day4'
 import { getLines, getDayData } from './utils'
 
 dotenv.config()
 
 async function run (): Promise<void> {
   const cookie = process.env.COOKIE
-  const dayData = await getDayData(3, cookie, './data')
+  const dayData = await getDayData(4, cookie, './data')
   const data = getLines(dayData)
 
-  const testDayData = `vJrwpWtwJgWrhcsFMMfFFhFp
-  jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-  PmmdzqPrVvPwwTWBwg
-  wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-  ttgJtRGJQctTZtZT
-  CrZsJsPPZsGzwwsLwLmpwMDw`
+  const testDayData = `2-4,6-8
+  2-3,4-5
+  5-7,7-9
+  2-8,3-7
+  6-6,4-6
+  2-6,4-8`
 
   const testData = getLines(testDayData).map(s => s.trim())
 
-  console.log(day3part1(data))
-  console.log(day3part2(data))
+  console.log(day4part1(data))
+  console.log(day4part2(data))
 }
 
 run().catch((error) => console.error(error))
